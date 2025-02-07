73e9880aafdde34cabe74baf2a71f1e6ff18da7c
Amitkumarkoli <akdev523@gmail.com>
2025-02-07 08:14:48 +0530

    creating addAt method in LL

--- Linked List/addLast.java
+++ Linked List/addLast.java
@@ -92,6 +92,28 @@ class LinkedList {
         }
         size++;
     }
+
+    public void addAt(int idx, int val){
+        if(idx < 0 || idx > size){
+            System.out.println("Invalid arguments");
+        }else if(idx == 0){
+            addFirst(val);
+        }else if(idx == size){
+            addLast(val);
+        }else {
+            Node node = new Node();
+            node.data = val;
+
+            Node temp = head;
+            for(int i = 0; i < idx-1; i++){
+                temp = temp.next;
+            }
+            node.next = temp.next;
+            temp.next = node;
+
+            size++;
+        }
+    }
 }
 
 public class addLast {  
@@ -134,5 +156,18 @@ public class addLast {
 
         System.out.println("New Linked List: ");
         list.display();
+
+        System.out.println("Enter index to add: ");
+        int index = Integer.parseInt(br.readLine().trim());
+
+        System.out.println("Enter element to add at index " + index + ": ");
+        int value = Integer.parseInt(br.readLine().trim());
+
+        list.addAt(index, value);
+
+        System.out.println("New Linked List: ");
+        list.display();
+        System.out.println("Linked List size:"+list.size());
+
     }
 }
