09469094ba11984398e9e8263197d70e5a021989
Amitkumarkoli <akdev523@gmail.com>
2025-02-07 08:30:08 +0530

    creating remove last from LL

--- Linked List/addLast.java
+++ Linked List/addLast.java
@@ -114,6 +114,23 @@ class LinkedList {
             size++;
         }
     }
+
+    public void removeLast(){
+         if(size==0){
+            System.out.println("List is empty");
+        }else if(size == 1){
+            head = tail = null;
+            size = 0;
+        }else {
+            Node temp = head;
+            for(int i = 0; i < size - 2; i++){
+                temp = temp.next;
+            }
+            tail = temp;
+            temp.next = null;
+            size--;
+        }
+    }
 }
 
 public class addLast {  
@@ -169,5 +186,10 @@ public class addLast {
         list.display();
         System.out.println("Linked List size:"+list.size());
 
+
+        list.removeLast();
+        System.out.println("New Linked List: ");
+        list.display();
+        System.out.println("Linked List size:"+list.size());
     }
 }
